b972e82e3dd3b9395439025c6a43e601dc47797d
lx <arthurlx94@163.com>
2025-09-21 16:21:19 +0800

    'update-sh'

--- reproduce_baselines.sh
+++ reproduce_baselines.sh
@@ -24,15 +24,8 @@ dataset="PEMS03 PEMS04 PEMS07 PEMS08"
 models="GWNet AGCRN DGCRN D2STGNN MTGNN BigST"
 
 for model in $models; do
-	for l in $input_length; do
-		for data in $dataset; do
-			for s in $seed; do
-				sed -i "s/INPUT_LEN = .*/INPUT_LEN = $l/" models/$model/$data.py
-				sed -i "s/OUTPUT_LEN = .*/OUTPUT_LEN = $output_length/" models/$model/$data.py
-				sed -i "s/CFG.ENV.SEED = .*/CFG.ENV.SEED = $s/" models/$model/$data.py
-				python experiments/train.py -c models/$model/$data.py -g 0 
-			done
-		done
+	for data in $dataset; do
+		python experiments/train.py -c baselines/$model/$data.py -g 0 
 	done
 	find checkpoints/ -type f -name "*.pt" | xargs rm -rf
 done
